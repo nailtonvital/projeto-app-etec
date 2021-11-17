0991b7c9a4daad30dbd070afa9dc16779a032688
Nailton <51789882+nailtonvital@users.noreply.github.com>
2021-11-16 22:25:30 -0300

    Telas e conexao de telas

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ ScreenManager:
         MDLabel:
             halign: "center"
             pos_hint: {'center_y': .75}
-            text: f"[font=C:/Users/Nailton/AppData/Local/Microsoft/Windows/Fonts/Montserrat-Regular]Brazil/Ecuador\\nTour[/font]"
+            text: f"[font=Fonts/Montserrat-Regular]Brazil/Ecuador\\nTour[/font]"
             markup: True
             font_size: 32
             
@@ -37,25 +37,27 @@ ScreenManager:
         # language
         MDRoundFlatButton:
             pos_hint: {'center_x': .5, 'center_y': .27}
-            text: f"[font=C:/Users/Nailton/AppData/Local/Microsoft/Windows/Fonts/Montserrat-Regular]Idioma[/font]"
+            text: f"[font=Fonts/Montserrat-Regular]Idioma[/font]"
             size_hint_x: 0.8
             on_release: app.show_confirmation_dialog()
+        
 
         # start button
         MDFillRoundFlatButton:
             pos_hint: {'center_x': .5, 'center_y': .2}
-            text: f"[font=C:/Users/Nailton/AppData/Local/Microsoft/Windows/Fonts/Montserrat-Regular]Começar Tour[/font]"
+            text: f"[font=Fonts/Montserrat-Regular]Começar Tour[/font]"
             size_hint_x: 0.8
-            on_release: manager.current = "home"
+            on_release: manager.current = "pais"
+        
             
             
     # country choose
     MDScreen:
-        name: "home"
+        name: "pais"
         
         MDToolbar:
             id: toolbar
-            title: f"[color=#ffffff][font=C:/Users/Nailton/AppData/Local/Microsoft/Windows/Fonts/Montserrat-Regular]Selecione um País:[/font][/color]"
+            title: f"[color=#ffffff][font=Fonts/Montserrat-Regular]Selecione um País:[/font][/color]"
             pos_hint: {"top": 1}
             md_bg_color: [0,0,1,0]
             anchor_title: "center"
@@ -84,6 +86,7 @@ ScreenManager:
                             opposite_colors: True
                             text: "\\nBrazil\\n"
                             size_hint_x: 1
+                            on_release: manager.current = "brasil"
             MDSwiperItem
                 RelativeLayout:
                     
@@ -104,9 +107,151 @@ ScreenManager:
                             size_hint_x: 1
                             
     #Brasil
-    MDScreen:
-        name: "brazil"
-        
+    MDScreen: 
+        name: "brasil"
+        MDFloatLayout:
+            MDFloatLayout:
+                mb_bg_color: 1,1,1,1
+                MDLabel:
+                    text: "Brazil"
+                    font_name: "Fonts/Poppins-SemiBold"
+                    font_size: "35sp"
+                    pos_hint: {"center_x": .56, "center_y": .92}
+            
+                MDLabel:
+                    text: "País"
+                    font_name: "Fonts/Poppins-Thin"
+                    font_size: "18sp"
+                    text_color: (102, 103, 105)
+                    pos_hint: {"center_x": .565, "center_y": .87}
+                    
+            MDLabel:
+                text: "Categorias"
+                font_name: "Fonts/Poppins-Regular"
+                font_size: "18sp"
+                text_color: (102, 103, 105)
+                pos_hint: {"center_x": .565, "center_y": .78}
+            
+            ScrollView:
+                do_scroll_y: False
+                do_scroll_x: True
+                pos_hint:{"center_y": .65}
+                size_hint_y: .2
+                bar_width: 0
+                GridLayout:
+                    size_hint_x: None
+                    height: self.minimum_height
+                    width: self.minimum_width
+                    rows: 1
+                    spacing: 8
+                    padding: 18, 0
+                    MDFillRoundFlatButton:
+                        text: f"[color=#ffffff] [font=Fonts/Montserrat-Regular]Culinária[/font] [/color]"
+                        on_release: manager.current="gallery"
+                    MDFillRoundFlatButton:
+                        text: f"[color=#ffffff] [font=Fonts/Montserrat-Regular]Costumes[/font] [/color]"
+                        
+                    MDFillRoundFlatButton:
+                        text: f"[color=#ffffff] [font=Fonts/Montserrat-Regular]Pontos Turisticos[/font] [/color]"
+                    
+                    MDFillRoundFlatButton:
+                        text: f"[color=#ffffff] [font=Fonts/Montserrat-Regular]Pessoas Influentes[/font] [/color]"
+                    
+            
+            MDLabel:
+                text: "Capitais"
+                font_name: "Fonts/Poppins-Regular"
+                font_size: "18sp"
+                text_color: (102, 103, 105)
+                pos_hint: {"center_x": .565, "center_y": .62}
+            ScrollView:
+                do_scroll_y: False
+                do_scroll_x: True
+                pos_hint:{"center_y": .44}
+                size_hint_y: .3
+                bar_width: 0
+                GridLayout:
+                    size_hint_x: None
+                    height: self.minimum_height
+                    width: self.minimum_width
+                    rows: 1
+                    spacing: 10
+                    padding: 18, 0
+                    Image:
+                        source: "images/Cidades/rj-mini.jpg"
+                        size_hint: None, None
+                        size: "120dp", "180dp"
+                        radius: [20,]
+                    Image:
+                        source: "images/Cidades/rj-mini.jpg"
+                        size_hint: None, None
+                        size: "120dp", "180dp"
+                        radius: [20,]
+                    Image:
+                        source: "images/Cidades/rj-mini.jpg"
+                        size_hint: None, None
+                        size: "120dp", "180dp"
+                        
+            MDLabel:
+                text: "Sudeste"
+                font_name: "Fonts/Poppins-Regular"
+                font_size: "18sp"
+                text_color: (102, 103, 105)
+                pos_hint: {"center_x": .565, "center_y": .25}
+    MDScreen: 
+        name: "gallery"
+        MDIconButton:
+            icon:"arrow-left"
+            pos_hint: {"center_y": .95}
+            on_release: manager.current = "brasil"
+        MDLabel:
+            id: title
+            text: "Comidas"
+            pos_hint: {"center_y": .95}
+            halign:"center"
+            font_name:"Fonts/Poppins-Regular"
+            font_size: "20sp"
+        ScrollView:
+            do_scroll_y: True
+            do_scroll_x: False
+            pos_hint:{"center_x": .5, "y": 0}
+            size_hint_y: .9
+            bar_width: 0
+            GridLayout:
+                size_hint_y: None
+                height: self.minimum_height
+                cols: 2
+                spacing: 2
+                padding: 10, 0
+                Image:
+                    source: ""
+                    size_hint: None, None
+                    size: "140dp", "200dp"
+                    radius: [20,]
+                Image:
+                    source: "kivy_venv/images/comidas/brigadeiro.jpg"
+                    size_hint: None, None
+                    size: "140dp", "200dp"
+                    radius: [20,]
+                Image:
+                    source: "kivy_venv/images/comidas/brigadeiro.jpg"
+                    size_hint: None, None
+                    size: "140dp", "200dp"
+                Image:
+                    source: "kivy_venv/images/comidas/brigadeiro.jpg"
+                    size_hint: None, None
+                    size: "140dp", "200dp"
+                    radius: [20,]
+                Image:
+                    source: "kivy_venv/images/comidas/brigadeiro.jpg"
+                    size_hint: None, None
+                    size: "140dp", "200dp"
+                    radius: [20,]
+                Image:
+                    source: "kivy_venv/images/comidas/brigadeiro.jpg"
+                    size_hint: None, None
+                    size: "140dp", "200dp"
+                    
 '''
 
 
--- teste.py
+++ teste.py
@@ -1,105 +1,159 @@
-from kivy.uix.modalview import ModalView
 from kivy.lang import Builder
-from kivy.loader import Loader
-from kivy.uix.image import AsyncImage
 from kivymd.app import MDApp
-from kivymd.uix.card import MDCard
 from kivy.core.window import Window
 
 Window.size = (300, 600)
 
 KV = '''
-MDScreen: 
-    MDFloatLayout:
+ScreenManager:
+    id: manager
+    MDScreen: 
+        name: "home"
         MDFloatLayout:
-            mb_bg_color: 1,1,1,1
+            MDFloatLayout:
+                mb_bg_color: 1,1,1,1
+                MDLabel:
+                    text: "Brazil"
+                    font_name: "Fonts/Poppins-SemiBold"
+                    font_size: "35sp"
+                    pos_hint: {"center_x": .56, "center_y": .92}
+            
+                MDLabel:
+                    text: "País"
+                    font_name: "Fonts/Poppins-Thin"
+                    font_size: "18sp"
+                    text_color: (102, 103, 105)
+                    pos_hint: {"center_x": .565, "center_y": .87}
+                    
             MDLabel:
-                text: "Brazil"
-                font_name: "C:/Users/Nailton/AppData/Local/Microsoft/Windows/Fonts/Poppins-SemiBold"
-                font_size: "35sp"
-                pos_hint: {"center_x": .56, "center_y": .92}
-        
-            MDLabel:
-                text: "País"
-                font_name: "C:/Users/Nailton/AppData/Local/Microsoft/Windows/Fonts/Poppins-Thin"
+                text: "Categorias"
+                font_name: "Fonts/Poppins-Regular"
                 font_size: "18sp"
                 text_color: (102, 103, 105)
-                pos_hint: {"center_x": .565, "center_y": .87}
-                
+                pos_hint: {"center_x": .565, "center_y": .78}
+            
+            ScrollView:
+                do_scroll_y: False
+                do_scroll_x: True
+                pos_hint:{"center_y": .65}
+                size_hint_y: .2
+                bar_width: 0
+                GridLayout:
+                    size_hint_x: None
+                    height: self.minimum_height
+                    width: self.minimum_width
+                    rows: 1
+                    spacing: 8
+                    padding: 18, 0
+                    MDFillRoundFlatButton:
+                        text: f"[color=#ffffff] [font=Fonts/Montserrat-Regular]Culinária[/font] [/color]"
+                        on_release: manager.current="gallery"
+                    MDFillRoundFlatButton:
+                        text: f"[color=#ffffff] [font=Fonts/Montserrat-Regular]Costumes[/font] [/color]"
+                        
+                    MDFillRoundFlatButton:
+                        text: f"[color=#ffffff] [font=Fonts/Montserrat-Regular]Pontos Turisticos[/font] [/color]"
+                    
+                    MDFillRoundFlatButton:
+                        text: f"[color=#ffffff] [font=Fonts/Montserrat-Regular]Pessoas Influentes[/font] [/color]"
+                    
+            
+            MDLabel:
+                text: "Capitais"
+                font_name: "Fonts/Poppins-Regular"
+                font_size: "18sp"
+                text_color: (102, 103, 105)
+                pos_hint: {"center_x": .565, "center_y": .62}
+            ScrollView:
+                do_scroll_y: False
+                do_scroll_x: True
+                pos_hint:{"center_y": .44}
+                size_hint_y: .3
+                bar_width: 0
+                GridLayout:
+                    size_hint_x: None
+                    height: self.minimum_height
+                    width: self.minimum_width
+                    rows: 1
+                    spacing: 10
+                    padding: 18, 0
+                    Image:
+                        source: "images/Cidades/rj-mini.jpg"
+                        size_hint: None, None
+                        size: "120dp", "180dp"
+                        radius: [20,]
+                    Image:
+                        source: "images/Cidades/rj-mini.jpg"
+                        size_hint: None, None
+                        size: "120dp", "180dp"
+                        radius: [20,]
+                    Image:
+                        source: "images/Cidades/rj-mini.jpg"
+                        size_hint: None, None
+                        size: "120dp", "180dp"
+                        
+            MDLabel:
+                text: "Sudeste"
+                font_name: "Fonts/Poppins-Regular"
+                font_size: "18sp"
+                text_color: (102, 103, 105)
+                pos_hint: {"center_x": .565, "center_y": .25}
+    MDScreen: 
+        name: "gallery"
+        MDIconButton:
+            icon:"arrow-left"
+            pos_hint: {"center_y": .95}
+            on_release: manager.current = "home"
         MDLabel:
-            text: "Categorias"
-            font_name: "C:/Users/Nailton/AppData/Local/Microsoft/Windows/Fonts/Poppins-Regular"
-            font_size: "18sp"
-            text_color: (102, 103, 105)
-            pos_hint: {"center_x": .565, "center_y": .78}
-        
+            id: title
+            text: "Comidas"
+            pos_hint: {"center_y": .95}
+            halign:"center"
+            font_name:"Fonts/Poppins-Regular"
+            font_size: "20sp"
         ScrollView:
-            do_scroll_y: False
-            do_scroll_x: True
-            pos_hint:{"center_y": .65}
-            size_hint_y: .2
+            do_scroll_y: True
+            do_scroll_x: False
+            pos_hint:{"center_x": .5, "y": 0}
+            size_hint_y: .9
             bar_width: 0
             GridLayout:
-                size_hint_x: None
+                size_hint_y: None
                 height: self.minimum_height
-                width: self.minimum_width
-                rows: 1
-                spacing: 8
-                padding: 18, 0
-                MDFillRoundFlatButton:
-                    text: f"[color=#ffffff] [font=C:/Users/Nailton/AppData/Local/Microsoft/Windows/Fonts/Montserrat-Regular]Culinária[/font] [/color]"
-                MDFillRoundFlatButton:
-                    text: f"[color=#ffffff] [font=C:/Users/Nailton/AppData/Local/Microsoft/Windows/Fonts/Montserrat-Regular]Costumes[/font] [/color]"
-                    
-                MDFillRoundFlatButton:
-                    text: f"[color=#ffffff] [font=C:/Users/Nailton/AppData/Local/Microsoft/Windows/Fonts/Montserrat-Regular]Pontos Turisticos[/font] [/color]"
-                
-                MDFillRoundFlatButton:
-                    text: f"[color=#ffffff] [font=C:/Users/Nailton/AppData/Local/Microsoft/Windows/Fonts/Montserrat-Regular]Pessoas Influentes[/font] [/color]"
-                
-        
-        MDLabel:
-            text: "Capitais"
-            font_name: "C:/Users/Nailton/AppData/Local/Microsoft/Windows/Fonts/Poppins-Regular"
-            font_size: "18sp"
-            text_color: (102, 103, 105)
-            pos_hint: {"center_x": .565, "center_y": .62}
-        ScrollView:
-            do_scroll_y: False
-            do_scroll_x: True
-            pos_hint:{"center_y": .44}
-            size_hint_y: .3
-            bar_width: 0
-            GridLayout:
-                size_hint_x: None
-                height: self.minimum_height
-                width: self.minimum_width
-                rows: 1
-                spacing: 10
-                padding: 18, 0
+                cols: 2
+                spacing: 2
+                padding: 10, 0
                 Image:
-                    source: "images/Cidades/rj-mini.jpg"
+                    source: ""
                     size_hint: None, None
-                    size: "120dp", "180dp"
+                    size: "140dp", "200dp"
                     radius: [20,]
                 Image:
-                    source: "images/Cidades/rj-mini.jpg"
+                    source: "kivy_venv/images/comidas/brigadeiro.jpg"
                     size_hint: None, None
-                    size: "120dp", "180dp"
+                    size: "140dp", "200dp"
                     radius: [20,]
                 Image:
-                    source: "images/Cidades/rj-mini.jpg"
+                    source: "kivy_venv/images/comidas/brigadeiro.jpg"
                     size_hint: None, None
-                    size: "120dp", "180dp"
+                    size: "140dp", "200dp"
+                Image:
+                    source: "kivy_venv/images/comidas/brigadeiro.jpg"
+                    size_hint: None, None
+                    size: "140dp", "200dp"
+                    radius: [20,]
+                Image:
+                    source: "kivy_venv/images/comidas/brigadeiro.jpg"
+                    size_hint: None, None
+                    size: "140dp", "200dp"
+                    radius: [20,]
+                Image:
+                    source: "kivy_venv/images/comidas/brigadeiro.jpg"
+                    size_hint: None, None
+                    size: "140dp", "200dp"
                     
-        MDLabel:
-            text: "Sudeste"
-            font_name: "C:/Users/Nailton/AppData/Local/Microsoft/Windows/Fonts/Poppins-Regular"
-            font_size: "18sp"
-            text_color: (102, 103, 105)
-            pos_hint: {"center_x": .565, "center_y": .25}
-        
                     
+                        
 '''
 
 
